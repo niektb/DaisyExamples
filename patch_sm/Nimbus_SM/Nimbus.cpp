cf30ceefdfc6754865674bc8e8b6b794d364ffda
mort13 <mo@redback.de>
2024-09-16 21:58:01 +0200

    added cv in for the main 4 parameters, added the modeswitch and the quality switch, doubled reserved memory

--- patch_sm/Nimbus_SM/Nimbus.cpp
+++ patch_sm/Nimbus_SM/Nimbus.cpp
@@ -8,39 +8,67 @@ using namespace patch_sm;
 
 GranularProcessorClouds processor;
 DaisyPatchSM              hw;
-Switch toggle;
-
+Switch toggle, mode_button;
 // Pre-allocate big blocks in main memory and CCM. No malloc here.
-uint8_t block_mem[118784];
-uint8_t block_ccm[65536 - 128];
+uint8_t block_mem[(118784*2)];
+uint8_t block_ccm[(65536*2) - 128];
 
 Parameters* parameters;
+
+int pbmode = 0;
+float led_mode = 5;
+int quality = 0;
+float led_quality = 0;
 
 void controls()
 {
     hw.ProcessAllControls();
     toggle.Debounce();
+    mode_button.Debounce();
+
     bool shift  = toggle.Pressed();
 
+    bool freezeGate = hw.gate_in_1.State();
+    parameters->freeze = freezeGate;
+
+    dsy_gpio_write(&hw.gate_out_1, freezeGate);
+    dsy_gpio_write(&hw.gate_out_2, freezeGate);
+
     if (shift == true)
-    {
-        hw.WriteCvOut(2,5);
-        
+    {   
+        if (mode_button.RisingEdge())
+        {
+            pbmode = (pbmode + 1) %4;
+            processor.set_playback_mode((PlaybackMode)pbmode);
+            led_mode = 5 - pbmode;
+        }
+        hw.WriteCvOut(2,led_mode);
+
         float posKnob = hw.GetAdcValue(CV_1);
-        parameters->position = posKnob;
+        float posCV = hw.GetAdcValue(CV_5);
+        parameters->position = DSY_CLAMP((posKnob + posCV),0,1);
 
         float sizeKnob = hw.GetAdcValue(CV_2);
-        parameters->size = fmap(sizeKnob, 0.1f, 0.99f); //too small values for the size leads to crashes.
+        float sizeCV = hw.GetAdcValue(CV_6);
+        parameters->size = DSY_CLAMP((sizeKnob + sizeCV),0.01,1); //too small values for the size leads to crashes.
 
         float densityKnob = hw.GetAdcValue(CV_3);
-        parameters->density = densityKnob;
+        float densityCV = hw.GetAdcValue(CV_7);
+        parameters->density = DSY_CLAMP((densityKnob + densityCV),0,1);
 
-        float dryWetKnob = hw.GetAdcValue(CV_4);
-        parameters->dry_wet = dryWetKnob;
+        float textureknob = hw.GetAdcValue(CV_4);
+        float textureCV = hw.GetAdcValue(CV_8);
+        parameters->texture = DSY_CLAMP((textureknob + textureCV),0,1);
     }
     else if (shift == false)
     {
-        hw.WriteCvOut(2,0);
+        if (mode_button.RisingEdge())
+        {
+            quality = (quality + 1) %4;
+            processor.set_quality((GrainQuality)quality);
+            led_quality = 1 + quality;
+        }
+        hw.WriteCvOut(2,led_quality);
 
         float pitchKnob = hw.GetAdcValue(CV_1);
         parameters->pitch = pitchKnob;
@@ -48,8 +76,8 @@ void controls()
         float panKnob = hw.GetAdcValue(CV_2);
         parameters->stereo_spread = panKnob;
 
-        float textureknob = hw.GetAdcValue(CV_3);
-        parameters->texture = textureknob;
+        float dryWetKnob = hw.GetAdcValue(CV_3);
+        parameters->dry_wet = dryWetKnob;
 
         float revKnob = hw.GetAdcValue(CV_4);
         parameters->reverb = revKnob;
@@ -88,10 +116,15 @@ int main(void)
 {
 
     hw.Init();
+    float sample_rate = hw.AudioSampleRate();
     toggle.Init(hw.B8);
+    mode_button.Init(hw.B7,
+                    sample_rate,
+                    Switch::TYPE_MOMENTARY,
+                    Switch::POLARITY_INVERTED,
+                    Switch::PULL_UP);
 
     hw.SetAudioBlockSize(32); // clouds won't work with blocks bigger than 32
-    float sample_rate = hw.AudioSampleRate();
     //init the luts
     InitResources(sample_rate);
 
