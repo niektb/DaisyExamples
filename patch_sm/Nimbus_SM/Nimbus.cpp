fc1c8e3a8b42bccabca1adefb175859ab41a41c4
Niek ten Brinke <niektb95@gmail.com>
2024-09-24 13:23:59 +0200

    Rearrange controls, add support for trigger, clamp all pots to avoid negative values, tweak led brightness, add feedback parameter

--- patch_sm/Nimbus_SM/Nimbus.cpp
+++ patch_sm/Nimbus_SM/Nimbus.cpp
@@ -16,9 +16,13 @@ uint8_t block_ccm[(65536*2) - 128];
 Parameters* parameters;
 
 int pbmode = 0;
-float led_mode = 5;
 int quality = 0;
-float led_quality = 0;
+float led_brightness = 0;
+
+float IndexToBrightness(int index, int total)
+{
+    return static_cast<float>(index + 1) / static_cast<float>(total);
+}
 
 void controls()
 {
@@ -31,59 +35,65 @@ void controls()
     bool freezeGate = hw.gate_in_1.State();
     parameters->freeze = freezeGate;
 
+    parameters->trigger = hw.gate_in_2.Trig();
+    bool Gate = hw.gate_in_2.State();
+    parameters->gate    = hw.gate_in_2.State();
+
     dsy_gpio_write(&hw.gate_out_1, freezeGate);
-    dsy_gpio_write(&hw.gate_out_2, freezeGate);
+    dsy_gpio_write(&hw.gate_out_2, Gate);
 
     if (shift == true)
     {   
         if (mode_button.RisingEdge())
         {
-            pbmode = (pbmode + 1) %4;
-            processor.set_playback_mode((PlaybackMode)pbmode);
-            led_mode = 5 - pbmode;
+            quality = (quality + 1) %4;
+            processor.set_quality((GrainQuality)quality);
+            led_brightness = IndexToBrightness(quality, 4);
         }
-        hw.WriteCvOut(2,led_mode);
 
         float posKnob = hw.GetAdcValue(CV_1);
         float posCV = hw.GetAdcValue(CV_5);
         parameters->position = DSY_CLAMP((posKnob + posCV),0,1);
 
         float sizeKnob = hw.GetAdcValue(CV_2);
-        float sizeCV = hw.GetAdcValue(CV_6);
-        parameters->size = DSY_CLAMP((sizeKnob + sizeCV),0.01,1); //too small values for the size leads to crashes.
+        parameters->size = DSY_CLAMP(sizeKnob,0.01,1); //too small values for the size leads to crashes.
 
         float densityKnob = hw.GetAdcValue(CV_3);
         float densityCV = hw.GetAdcValue(CV_7);
         parameters->density = DSY_CLAMP((densityKnob + densityCV),0,1);
 
-        float textureknob = hw.GetAdcValue(CV_4);
-        float textureCV = hw.GetAdcValue(CV_8);
-        parameters->texture = DSY_CLAMP((textureknob + textureCV),0,1);
+        float pitchKnob = hw.GetAdcValue(CV_4);
+        float pitchCV = hw.GetAdcValue(CV_8);
+        float val = DSY_CLAMP((pitchKnob+pitchCV), 0, 1);
+        parameters->pitch = powf(9.798f * (val - .5f), 2.f);
+        parameters->pitch *= val < .5f ? -1.f : 1.f;
     }
     else if (shift == false)
     {
         if (mode_button.RisingEdge())
         {
-            quality = (quality + 1) %4;
-            processor.set_quality((GrainQuality)quality);
-            led_quality = 1 + quality;
+            pbmode = (pbmode + 1) % 4;
+            processor.set_playback_mode((PlaybackMode)pbmode);
+            led_brightness = IndexToBrightness(pbmode, 4);
         }
-        hw.WriteCvOut(2,led_quality);
 
-        float pitchKnob = hw.GetAdcValue(CV_1);
-        parameters->pitch = pitchKnob;
+        float textureknob = hw.GetAdcValue(CV_1);
+        parameters->texture = DSY_CLAMP(textureknob, 0, 1);
 
-        float panKnob = hw.GetAdcValue(CV_2);
-        parameters->stereo_spread = panKnob;
+        float dryWetKnob = hw.GetAdcValue(CV_2);
+        float dryWetCV = hw.GetAdcValue(CV_6);
+        parameters->dry_wet = DSY_CLAMP((dryWetKnob + dryWetCV),0,1);
 
-        float dryWetKnob = hw.GetAdcValue(CV_3);
-        parameters->dry_wet = dryWetKnob;
+        float feedbackKnob = hw.GetAdcValue(CV_3);
+        parameters->feedback = DSY_CLAMP(feedbackKnob, 0.01f, 1.f);
 
         float revKnob = hw.GetAdcValue(CV_4);
-        parameters->reverb = revKnob;
-    }
-}
+        parameters->reverb = DSY_CLAMP(revKnob, 0, 1);
 
+        parameters->stereo_spread = 1.0f; // not mapped, preset to a max spread
+    }
+    hw.WriteCvOut(CV_OUT_2, led_brightness * 5.f);
+}
 
 void AudioCallback(AudioHandle::InputBuffer  in,
                    AudioHandle::OutputBuffer out,
@@ -135,6 +145,17 @@ int main(void)
                    sizeof(block_ccm));
 
     parameters = processor.mutable_parameters();
+
+    // initialize to avoid big jumps when toggling B8 for the first time.
+    parameters->density = .5f;
+    parameters->feedback = .01f;
+    parameters->dry_wet = .5f;
+    parameters->texture = .5f;
+    parameters->pitch = .5f;
+    parameters->size = .5f;
+    parameters->reverb = .0f;
+    parameters->position = .5f;
+
     hw.StartAdc();
     hw.StartAudio(AudioCallback);
     while(1)
